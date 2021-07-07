7956cf1451e5072c83f4699531293995352e4f67
Meatware Monster <meatwaremonster@gmail.com>
2021-07-07 02:38:45 -0400

    Fix api versioning.

--- backend/ValheimBlueprintStore/Startup.cs
+++ backend/ValheimBlueprintStore/Startup.cs
@@ -1,11 +1,16 @@
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using ValheimBlueprintStore.Models;
 
 namespace ValheimBlueprintStore
@@ -24,16 +29,25 @@ namespace ValheimBlueprintStore
         {
             services.AddDbContext<TodoContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("Default")));
             services.AddControllers();
-            services.AddApiVersioning(config =>
+            services.AddApiVersioning(options =>
             {
-                config.DefaultApiVersion = new ApiVersion(1, 0);
-                config.AssumeDefaultVersionWhenUnspecified = true;
-                config.ReportApiVersions = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
             });
-            services.AddSwaggerGen(c =>
+
+            services.AddVersionedApiExplorer(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ValheimBlueprintStore", Version = "v1" });
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service  
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"  
+                options.GroupNameFormat = "'v'VVV";
+
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat  
+                // can also be used to control the format of the API version in route templates  
+                options.SubstituteApiVersionInUrl = true;
             });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options => options.OperationFilter<SwaggerDefaultValues>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +70,69 @@ namespace ValheimBlueprintStore
             {
                 endpoints.MapControllers();
             });
+        }
+
+        public class SwaggerDefaultValues : IOperationFilter
+        {
+            public void Apply(OpenApiOperation operation, OperationFilterContext context)
+            {
+                var apiDescription = context.ApiDescription;
+                operation.Deprecated |= apiDescription.IsDeprecated();
+
+                if (operation.Parameters == null)
+                    return;
+
+                // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/412
+                // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/pull/413
+                foreach (var parameter in operation.Parameters)
+                {
+                    var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+                    if (parameter.Description == null)
+                    {
+                        parameter.Description = description.ModelMetadata?.Description;
+                    }
+
+                    if (parameter.Schema.Default == null && description.DefaultValue != null)
+                    {
+                        parameter.Schema.Default = new OpenApiString(description.DefaultValue.ToString());
+                    }
+
+                    parameter.Required |= description.IsRequired;
+                }
+            }
+        }
+
+        public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+        {
+            private readonly IApiVersionDescriptionProvider _provider;
+
+            public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+            public void Configure(SwaggerGenOptions options)
+            {
+                // add a swagger document for each discovered API version
+                // note: you might choose to skip or document deprecated API versions differently
+                foreach (var description in _provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                }
+            }
+
+            private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+            {
+                var info = new OpenApiInfo()
+                {
+                    Title = "Sample API",
+                    Version = description.ApiVersion.ToString(),
+                };
+
+                if (description.IsDeprecated)
+                {
+                    info.Description += " This API version has been deprecated.";
+                }
+
+                return info;
+            }
         }
     }
 }
